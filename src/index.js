ec2b3eba1d8ce81d8c1760911ead4e93c153b75a
Akaradech Sutheesuntornkul <iamakaradech@gmail.com>
2019-09-04 22:17:51 +0700

    feat(unique-number): add function uniqueNumber

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+let data = [ 1, 1, 1, 2, 1, 1, 3, 3, 4, 4, 6, 6, 3, 10, 10, 1, 2, 8, 8, 'A' ]
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const uniqueNumber = (arr) => {
+  let processed = []
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    if (processed.indexOf(item) >= 0) {
+      continue
+    }
+
+    processed.push(item)
+
+    result.push({ number: item, count: arr.filter((v) => v === item).length })
+  }
+
+  let unique = result.filter((item) => item.count === 1)
+
+  if (!unique.length) {
+    return null
+  }
+
+  return unique[0].number
+}
+
+console.log(uniqueNumber(data))
