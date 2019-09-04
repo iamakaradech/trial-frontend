7b4064c2c4f9e7aa871b02a18e9058a44dc2b975
Akaradech Sutheesuntornkul <iamakaradech@gmail.com>
2019-09-04 22:34:54 +0700

    feat(spread-rest): add example rest

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
-let data = [ 1, 1, 1, 2, 1, 1, 3, 3, 4, 4, 6, 6, 3, 10, 10, 1, 2, 8, 8, 'A' ]
-
-const uniqueNumber = (arr) => {
-  let processed = []
-  let result = []
-  for (let i = 0; i < arr.length; i++) {
-    let item = arr[i]
-
-    if (processed.indexOf(item) >= 0) {
-      continue
-    }
-
-    processed.push(item)
-
-    result.push({ number: item, count: arr.filter((v) => v === item).length })
-  }
-
-  let unique = result.filter((item) => item.count === 1)
-
-  if (!unique.length) {
-    return null
-  }
-
-  return unique[0].number
+/*
+* Function can accept any parameter but first parameter is member and another are in args variable
+*/
+const acceptAnyParameters = (member, ...args) => {
+  console.log('Name: ', member)
+  console.log('Another argrument: ', args)
 }
 
-console.log(uniqueNumber(data))
+const cars = ['Corvette Z06', 'Lotus Exite S', 'Honda Jazz', 'Honda Click', 'Honda Waves']
+// Copy value from cars into member's cars property
+const member = { name: 'Rick', cars: [...cars] }
+
+// Pass member object into function and another parameter
+acceptAnyParameters(member, 'red', 'bold')
